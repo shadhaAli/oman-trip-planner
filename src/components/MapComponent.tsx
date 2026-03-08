52eadf771f9f7a2e85d75a935bba5bb341cf1300
Shadha Ali <shadhaali1234@gmail.com>
2026-03-08 22:23:39 +0400

    Add start and end icons to map markers
    - Added start.png and end.png icons for map markers
    - Updated MapComponent to display start icon for first place
    - Updated MapComponent to display end icon for last place
    - Used default mountain icon for intermediate places

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -3,7 +3,28 @@
 import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
 import { Destination } from "../types/destination";
 import "leaflet/dist/leaflet.css";
-import { LatLngExpression } from "leaflet";
+import { LatLngExpression, Icon } from "leaflet";
+
+const startIcon = new Icon({
+  iconUrl: "/icon/start.png",
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32]
+});
+
+const endIcon = new Icon({
+  iconUrl: "/icon/end.png",
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32]
+});
+
+const defaultIcon = new Icon({
+  iconUrl: "/icon/mountain.png",
+  iconSize: [24, 24],
+  iconAnchor: [12, 24],
+  popupAnchor: [0, -24]
+});
 
 function MapComponent({ places }: { places: Destination[] }) {
   const route: LatLngExpression[] = places.map(p => [p.lat, p.lng] as LatLngExpression);
@@ -19,11 +40,20 @@ function MapComponent({ places }: { places: Destination[] }) {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      {places.map(place => (
-        <Marker key={place.id} position={[place.lat, place.lng]}>
-          <Popup>{place.name.en}</Popup>
-        </Marker>
-      ))}
+      {places.map((place, index) => {
+        let icon = defaultIcon;
+        if (index === 0 && places.length > 1) {
+          icon = startIcon;
+        } else if (index === places.length - 1 && places.length > 1) {
+          icon = endIcon;
+        }
+        
+        return (
+          <Marker key={place.id} position={[place.lat, place.lng]} icon={icon}>
+            <Popup>{place.name.en}</Popup>
+          </Marker>
+        );
+      })}
 
       <Polyline positions={route} />
     </MapContainer>
